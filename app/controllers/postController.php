5692fd8bc7652b214d2e08599611198bca17336a
Eros <garotoslixo@gmail.com>
2024-02-13 21:09:32 -0300

    Comments System

--- app/controllers/postController.php
+++ app/controllers/postController.php
@@ -16,6 +16,8 @@
                 $args['title'] = $collect->title;
                 $args['content'] = $collect->content;
                 $args['comments'] = $collect->comments;
+                $args['id'] = $collect->id;
+
 
                 $content = $template->render($args);
 
@@ -24,6 +26,19 @@
             catch (Exception $e)
             {
                 echo $e->getMessage();
+            }
+        }
+
+        
+        public Function addComment()
+        {
+            try {
+                Comments::addComments($_POST);
+
+                header("Location: ?page=post&id=" . $_POST['id']);
+            } catch (Exception $e) {
+                echo '<script>alert("'.$e->getMessage().'");</script>';
+                echo '<script>location.href="?page=post&id='.$_POST['id'].'"</script>';
             }
         }
     }
--- app/model/comments.php
+++ app/model/comments.php
@@ -5,7 +5,7 @@
         {
             $conn = Connection::getConn();
 
-            $sql = "SELECT * FROM comments WHERE id_post = :id";
+            $sql = "SELECT * FROM comments WHERE id_post = :id ORDER BY id DESC";
             $sql = $conn->prepare($sql);
             $sql->bindValue(':id',$idPost, PDO::PARAM_INT);
             $sql->execute();
@@ -18,6 +18,25 @@
             }
 
             return $res;
+        }
+
+        public static function addComments($data)
+        {
+            $conn = Connection::getConn();
+
+            $sql = "INSERT INTO comments (name, message, id_post) VALUES (:name, :message, :id)";
+            $sql = $conn->prepare($sql);
+            $sql->bindValue(':name',$data['name']);
+            $sql->bindValue(':message',$data['msg']);            
+            $sql->bindValue(':id',$data['id']);            
+            $sql->execute();
+
+            if ($sql->rowCount())
+            {
+                return true;
+            }
+
+            throw new Exception("Falha na inserção.");
         }
     }
 ?>
